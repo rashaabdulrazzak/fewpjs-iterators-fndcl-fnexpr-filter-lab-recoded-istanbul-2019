6e82f186ccc640b8331d1efce1d9281705aa5863
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-31 21:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+let drivers = ["Ahmed","JOHN","Rasha"]
+function findMatching(arr,str){
+  let result = arr.filter( name => (name === str));
+  if(result.length !== 0) {
+    return result;
+  }
+  else {
+   return arr.filter( name => name.toLowercase() === str.toLowercase());
+    }
+  }
+  
