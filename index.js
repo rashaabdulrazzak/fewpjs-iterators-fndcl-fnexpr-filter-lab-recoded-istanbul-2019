14489c8be9a38b67a8e3e3e8cc5e09e0f2c83893
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-31 21:31:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // Code your solution here
 let drivers = ["Ahmed","JOHN","Rasha"]
 function findMatching(arr,str){
-  let result = arr.filter( name => (name === str));
-  if(result.length != 0) {
-    return result;
-  }
-  else {
-   return arr.filter( name => name.toLowerCase() === str.toLowerCase());
-    }
-  }
+  let result = arr.filter( name => name.toLowerCase() === str.toLowerCase());
+  return result;
+  
+}
   
