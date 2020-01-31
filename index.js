81573d08f27f35f838a9893ab690c655cb3e8e77
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-31 21:41:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+let drivers = ["Ahmed","JOHN","Rasha"]
+function findMatching(arr,str){
+  let result = arr.filter( name => name.toLowerCase() === str.toLowerCase());
+  return result;
+}
+function fuzzyMatch(arr,str){
+  return arr.filter(name => name.startsWith(str));
+}
+function matchName(arr,str){
+    return  arr.filter(( {name} )=> name.toLowerCase() === str.toLowerCase());
+
+}
