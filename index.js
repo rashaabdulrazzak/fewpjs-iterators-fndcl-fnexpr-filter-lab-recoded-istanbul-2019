a9676264617b3a040ee0d1ec1eb1bbab1b6a8c6e
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-31 21:34:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function findMatching(arr,str){
   let result = arr.filter( name => name.toLowerCase() === str.toLowerCase());
   return result;
 }
-function fuzzyMatch(){}
+function fuzzyMatch(arr,str){
+  return arr.filter(name => name.startWith(s));
+}
